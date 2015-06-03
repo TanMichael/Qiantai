5b6f7303578de596f43e470416d059d6d898459a
zhaoluquan <zhaoluquan@qq.com>
2015-06-03 22:13:14 +0800

    生产单打印

--- QTsys/打印制造单.Designer.cs
+++ QTsys/打印制造单.Designer.cs
@@ -53,6 +53,12 @@
             this.webBrowser2 = new System.Windows.Forms.WebBrowser();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // com生产状态
@@ -67,6 +73,7 @@
             // label14
             // 
             this.label14.AutoSize = true;
+            this.label14.Enabled = false;
             this.label14.Location = new System.Drawing.Point(250, 91);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(65, 12);
@@ -85,6 +92,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
+            this.label10.Enabled = false;
             this.label10.Location = new System.Drawing.Point(17, 139);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(53, 12);
@@ -102,6 +110,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
+            this.label9.Enabled = false;
             this.label9.Location = new System.Drawing.Point(226, 115);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(89, 12);
@@ -120,6 +129,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
+            this.label8.Enabled = false;
             this.label8.Location = new System.Drawing.Point(250, 65);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(65, 12);
@@ -137,6 +147,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.Enabled = false;
             this.label7.Location = new System.Drawing.Point(226, 41);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(89, 12);
@@ -154,6 +165,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.Enabled = false;
             this.label6.Location = new System.Drawing.Point(250, 14);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(65, 12);
@@ -171,6 +183,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Enabled = false;
             this.label5.Location = new System.Drawing.Point(5, 115);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(65, 12);
@@ -188,6 +201,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Enabled = false;
             this.label4.Location = new System.Drawing.Point(5, 91);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(65, 12);
@@ -206,6 +220,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Enabled = false;
             this.label3.Location = new System.Drawing.Point(5, 62);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(65, 12);
@@ -224,6 +239,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Enabled = false;
             this.label2.Location = new System.Drawing.Point(5, 36);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 12);
@@ -241,6 +257,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Enabled = false;
             this.label1.Location = new System.Drawing.Point(26, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 12);
@@ -259,17 +276,17 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(642, 124);
+            this.button2.Location = new System.Drawing.Point(491, 115);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(113, 23);
             this.button2.TabIndex = 170;
-            this.button2.Text = "打印预览";
+            this.button2.Text = "生成新生产单";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(642, 91);
+            this.button1.Location = new System.Drawing.Point(648, 115);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 169;
@@ -277,11 +294,73 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(536, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 21);
+            this.textBox1.TabIndex = 171;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Enabled = false;
+            this.label11.Location = new System.Drawing.Point(489, 65);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 12);
+            this.label11.TabIndex = 172;
+            this.label11.Text = "备注：";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Enabled = false;
+            this.label12.Location = new System.Drawing.Point(465, 41);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(65, 12);
+            this.label12.TabIndex = 174;
+            this.label12.Text = "可否超交：";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Enabled = false;
+            this.label13.Location = new System.Drawing.Point(465, 11);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(65, 12);
+            this.label13.TabIndex = 176;
+            this.label13.Text = "包装要求：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(536, 8);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 21);
+            this.textBox3.TabIndex = 175;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "是",
+            "否"});
+            this.comboBox1.Location = new System.Drawing.Point(536, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 177;
+            // 
             // 打印制造单
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(749, 518);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.webBrowser2);
@@ -342,5 +421,11 @@
         private System.Windows.Forms.WebBrowser webBrowser2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- QTsys/打印制造单.cs
+++ QTsys/打印制造单.cs
@@ -21,12 +21,14 @@ namespace QTsys
     {
         private bool isnewplan;
         private string planid;
+        private ProductPlanDAO pdao;
 
         public 打印制造单()
         {
             InitializeComponent();
             planid = "";
             isnewplan = true;
+            pdao = new ProductPlanDAO();
         }
 
         public 打印制造单(string planID)
@@ -34,10 +36,13 @@ namespace QTsys
             InitializeComponent();
            planid = planID;
             isnewplan = false;
+            pdao = new ProductPlanDAO();
         }
 
         private void 打印制造单_Load(object sender, EventArgs e)
         {
+            text编号.Text = planid;
+            comboBox1.Text = "否";
             if (isnewplan == true)
             {
                 webBrowser2.Url = new Uri(Directory.GetCurrentDirectory() + "\\各种单据\\生产单.htm");
@@ -53,14 +58,52 @@ namespace QTsys
         private void button1_Click(object sender, EventArgs e)
         {
             //webBrowser2.Print();
-            webBrowser2.ShowPrintDialog(); 
-            //webBrowser2.ShowPageSetupDialog();
+           // webBrowser2.ShowPrintDialog(); 
+            //webBrowser2.ShowPageSetupDialog(); 
+            webBrowser2.ShowPageSetupDialog();
+            webBrowser2.ShowPrintPreviewDialog();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            webBrowser2.ShowPageSetupDialog();
-           webBrowser2.ShowPrintPreviewDialog();
+            //webBrowser2.ShowPrintPreviewDialog();
+            insetnewlist();
+        }
+
+        public void insetnewlist()//生成新的工程制作指示单
+        {
+            try
+            {
+                StreamReader rd = new StreamReader(Directory.GetCurrentDirectory() + "\\各种单据\\生产单_DATA.htm", Encoding.GetEncoding("gb2312"));
+                string usedata = rd.ReadToEnd();
+                rd.Close();
+                //把数据读入usedata
+               //**替换操作*************************************************************
+               DataTable ds=new DataTable();
+                ds=pdao.GetAllProductPlanByNameEX("编号",text编号.Text);
+                usedata = usedata.Replace("《下单编号》", text编号.Text);
+                usedata = usedata.Replace("《开单日期》", ds.Rows[0]["下单日期"].ToString());
+                usedata = usedata.Replace("《客编》", ds.Rows[0]["客户编号"].ToString());
+                usedata = usedata.Replace("《客户料号》", "  ");
+                usedata = usedata.Replace("《工程单编号》", ds.Rows[0]["产品编号"].ToString());
+                usedata = usedata.Replace("《订单号》", ds.Rows[0]["相关订单编号"].ToString());
+                usedata = usedata.Replace("《订单数量》", ds.Rows[0]["产品数量"].ToString());
+                usedata = usedata.Replace("《交货日期》", ds.Rows[0]["交付时间"].ToString());
+                usedata = usedata.Replace("《包装要求》", textBox3.Text);
+                usedata = usedata.Replace("《可否超交》", comboBox1.Text);
+                usedata = usedata.Replace("《备注》", textBox1.Text);
+                
+                //把usedata写入生产单_OVER并更新
+                File.Delete(Directory.GetCurrentDirectory() + "\\各种单据\\生产单_OVER.htm");
+                StreamWriter wr = new StreamWriter(Directory.GetCurrentDirectory() + "\\各种单据\\生产单_OVER.htm", true, Encoding.GetEncoding("gb2312"));
+                wr.Write(usedata);
+                wr.Close();
+                webBrowser2.Url = new Uri(Directory.GetCurrentDirectory() + "\\各种单据\\生产单_OVER.htm");//显示网页
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("请先选择一个产品！");
+            }
         }
     }
 }
