96032e3c3d6dad6d064f03446c05cb99d4e895c8
zhaoluquan <zhaoluquan@qq.com>
2015-06-15 21:29:38 +0800

    XLS导出功能，销售情况汇总

--- QTsys/销售管理.Designer.cs
+++ QTsys/销售管理.Designer.cs
@@ -58,6 +58,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -311,11 +312,22 @@
             this.button2.Visible = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(860, 633);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 23);
+            this.button3.TabIndex = 91;
+            this.button3.Text = "导出此表为EXCEL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // 销售管理
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(991, 639);
+            this.ClientSize = new System.Drawing.Size(991, 659);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox9);
             this.Controls.Add(this.button1);
@@ -387,5 +399,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- QTsys/销售管理.cs
+++ QTsys/销售管理.cs
@@ -11,6 +11,8 @@ using QTsys.Common;
 using QTsys.DataObjects;
 using QTsys.DAO;
 using QTsys.Manager;
+//using Microsoft.Office.Interop.Excel;
+using System.Data.OleDb;
 
 namespace QTsys
 {
@@ -48,6 +50,82 @@ namespace QTsys
         {
             打印送货单 win = new 打印送货单(textBox3.Text);
             win.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = (DataTable)dataGridView1.DataSource;
+            ExportExcel(dt);
+        }
+
+        private void ExportExcel(DataTable mycsvdt)
+        {
+            if (mycsvdt == null || mycsvdt.Rows.Count < 0)
+            {
+                return;
+            }
+            bool fileSaved = false;
+            SaveFileDialog sfdSaveFile = new SaveFileDialog();
+            //设置保存文件的格式
+            sfdSaveFile.DefaultExt = "xlsx";
+            //sfdSaveFile.DefaultExt = "xls";
+            sfdSaveFile.Filter = "Excel文件(*.xlsx)|*.xlsx|Excel文件(*.xls)|*.xls";
+            sfdSaveFile.FileName = string.Empty;
+            if (sfdSaveFile.ShowDialog() != DialogResult.OK) return;
+            //电脑Excel程序
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            if (xlApp == null)
+            {
+                MessageBox.Show("无法创建Excel对象，可能您的机子未安装Excel程序！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Workbook集合
+            Microsoft.Office.Interop.Excel.Workbooks workbooks = xlApp.Workbooks;
+            //Workbook
+            Microsoft.Office.Interop.Excel.Workbook workbook = workbooks.Add(Microsoft.Office.Interop.Excel.XlWBATemplate.xlWBATWorksheet);
+            //WorkSheet
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Worksheets[1];//取得sheet1 
+            //写入字段列标题 
+            for (int i = 0; i < mycsvdt.Columns.Count; i++)
+            {
+                worksheet.Cells[1, i + 1] = mycsvdt.Columns[i].ColumnName;
+            }
+            //写入数值 
+            for (int r = 0; r < mycsvdt.Rows.Count; r++)
+            {
+                for (int i = 0; i < mycsvdt.Columns.Count; i++)
+                {
+                    worksheet.Cells[r + 2, i + 1] = mycsvdt.Rows[r][i];
+                }
+                System.Windows.Forms.Application.DoEvents();
+            }
+            worksheet.Columns.EntireColumn.AutoFit();//列宽自适应。
+            //对指定列进行格式输出
+            //Microsoft.Office.Interop.Excel.Range rg = worksheet.get_Range(worksheet.Cells[2, 1], worksheet.Cells[this.table.Rows.Count + 1, 1]);
+            //rg.NumberFormat = "00000000";    
+            try
+            {
+                workbook.Saved = true;
+                workbook.SaveCopyAs(sfdSaveFile.FileName);//保存复制到指定位置
+                fileSaved = true;
+            }
+            catch (Exception ex)
+            {
+                fileSaved = false;
+                MessageBox.Show("导出文件时出错,文件可能正被打开！\n" + ex.Message);
+            }
+            finally
+            {
+                workbooks.Close();
+                xlApp.Quit();
+                GC.Collect();//强行销毁 
+            }
+            if (fileSaved && System.IO.File.Exists(sfdSaveFile.FileName))
+            {
+                //System.IO.File.Open(sfdSaveFile.FileName, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Read);
+                System.Diagnostics.Process.Start(sfdSaveFile.FileName); //打开EXCEL
+            }
         }
     }
 }
